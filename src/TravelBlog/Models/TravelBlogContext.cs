08d03bf111f13a61c05a435ec3d48107bfa443d1
Joshua Fairchild and Marilyn Carlin <joshua+marilyn@>
2017-08-02 08:20:48 -0700

    implement database migration

--- src/TravelBlog/Models/TravelBlogContext.cs
+++ src/TravelBlog/Models/TravelBlogContext.cs
@@ -5,6 +5,10 @@ namespace TravelBlog.Models
 {
     public class TravelBlogContext : DbContext
     {
+
+        public TravelBlogContext()
+        {
+        }
         public virtual DbSet<Experience> Experiences { get; set; }
         public virtual DbSet<Location> Locations { get; set; }
         public virtual DbSet<Person> People { get; set; }
@@ -12,6 +16,14 @@ namespace TravelBlog.Models
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
             options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=TravelBlog;integrated security=True");
+        }
+        public TravelBlogContext(DbContextOptions<TravelBlogContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
         }
     }
 }
