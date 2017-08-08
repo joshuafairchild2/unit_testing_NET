2d75b142d81d23e0c9c25474ed788b61939ab3c5
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-08-08 13:47:17 -0700

    add a few more tests

---
+++ TravelBlog.Tests/ModelTests/PersonControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using TravelBlog.Controllers;
+using TravelBlog.Models;
+using Xunit;
+
+namespace TravelBlog.Tests
+{
+    public class PersonControllerTest
+    {
+        [Fact]
+        public void Get_ViewResult_Index_Test()
+        {
+            PeopleController controller = new PeopleController();
+
+            IActionResult result = controller.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void Get_ModelList_Index_Test()
+        {
+            ViewResult indexView = new PeopleController().Index() as ViewResult;
+
+            object result = indexView.ViewData.Model;
+
+            Assert.IsType<List<Person>>(result);
+        }
+    }
+}
--- src/TravelBlog/Models/Person.cs
+++ src/TravelBlog/Models/Person.cs
@@ -6,10 +6,31 @@ namespace TravelBlog.Models
     [Table("People")]
     public class Person
     {
+
         [Key]
         public int PeopleId { get; set; }
         public string Name { get; set; }
         public int ExperienceId { get; set; }
         public virtual Experience Experience { get; set; }
+
+
+
+        public override bool Equals(System.Object otherPerson)
+        {
+            if (!(otherPerson is Person))
+            {
+                return false;
+            }
+            else
+            {
+                Person newPerson = (Person)otherPerson;
+                return this.PeopleId.Equals(newPerson.PeopleId);
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return this.PeopleId.GetHashCode();
+        }
     }
 }
