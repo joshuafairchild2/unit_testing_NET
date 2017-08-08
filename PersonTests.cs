5a802b1a9bab470a2a47a6ebfe02fcd948f54c6a
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-08-08 11:36:38 -0700

    set up unit testing and add first test for Person class

---
+++ PersonTests.cs
@@ -0,0 +1,21 @@
+﻿using TravelBlog.Models;
+using Xunit;
+
+namespace TravelBlog.Tests
+{
+    public class PersonTests
+    {
+        [Fact]
+        public void GetNameTest()
+        {
+            //Arrange
+            var person = new Person();
+
+            //Act
+            var result = person.Name;
+
+            //Assert
+            Assert.Equal("Joshua", result);
+        }
+    }
+}
---
+++ TravelBlog.Tests/PersonTest.cs
@@ -0,0 +1,19 @@
+﻿using TravelBlog.Models;
+using Xunit;
+
+namespace TravelBlog.Tests
+{
+    public class PersonTests
+    {
+        [Fact]
+        public void GetNameTest()
+        {
+            Person person = new Person();
+            person.Name = "Joshua";
+
+            string result = person.Name;
+
+            Assert.Equal("Joshua", result);
+        }
+    }
+}
